3c01b3111e10173fcf5882e65f8ceb156ddf0aca
Joao Pedro Almeida Moreira <joaoalmeida0913@outlook.com>
2025-07-21 14:01:47 -0300

    Criação do erroHandler e implementação do mesmo no server.js

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require(`express`);
 const app = express();
+const errorHandler = require(`./utils/errorHandler`);
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swagger');
+
+
 const PORT = 3000;
+
 
 const agentesRouter = require(`./routes/agentesRoutes`);
 const casosRouter = require(`./routes/casosRoutes`);
 
+
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+
 app.use(agentesRouter);
 app.use(casosRouter);
+
+app.use(errorHandler);
+
 
 app.listen(PORT, () =>{
     console.log(`Servidor do Departamento de Polícia rodando em localhost:${PORT}`);
